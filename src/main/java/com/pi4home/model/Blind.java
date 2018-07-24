912a2005233c26cb5a47d38e206e81b6dbd7b3b7
Pawel <pawel.grzyb1985@gmail.com>
2018-07-24 22:34:34 +0200

    added Blind object to represent blind state and pins for steering blind up and down

---
+++ src/main/java/com/pi4home/model/Blind.java
@@ -0,0 +1,52 @@
+package com.pi4home.model;
+
+import com.pi4home.enums.BlindState;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+
+public class Blind
+{
+    GpioPinDigitalOutput goUpPin;
+    GpioPinDigitalOutput goDownPin;
+    BlindState blindState;
+    String name;
+
+    public GpioPinDigitalOutput getGoUpPin()
+    {
+        return goUpPin;
+    }
+
+    public void setGoUpPin(GpioPinDigitalOutput goUpPin)
+    {
+        this.goUpPin = goUpPin;
+    }
+
+    public GpioPinDigitalOutput getGoDownPin()
+    {
+        return goDownPin;
+    }
+
+    public void setGoDownPin(GpioPinDigitalOutput goDownPin)
+    {
+        this.goDownPin = goDownPin;
+    }
+
+    public BlindState getBlindState()
+    {
+        return blindState;
+    }
+
+    public void setBlindState(BlindState blindState)
+    {
+        this.blindState = blindState;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+}
