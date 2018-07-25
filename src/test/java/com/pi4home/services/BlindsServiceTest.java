3c26f0a2f76e87b844bfac606037d81968c857bc
Pawel <pawel.grzyb1985@gmail.com>
2018-07-25 22:11:14 +0200

    fixed unit test

--- src/test/java/com/pi4home/services/BlindsServiceTest.java
+++ src/test/java/com/pi4home/services/BlindsServiceTest.java
@@ -1,22 +1,38 @@
 package com.pi4home.services;
 
+import com.pi4home.controller.RootController;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class BlindsServiceTest
 {
-    @Autowired
-    BlindsService blindsService;
+    private static MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup()
+    {
+        this.mockMvc = standaloneSetup(new RootController()).build();
+    }
 
     @Test
-    public void getPinByNameReturnsPin() throws Exception
+    public void testAppIsUpAndRunning() throws Exception
     {
-        blindsService.toggleBlindState("pinBlindLargeWindowLeft");
+        this.mockMvc.perform(get("/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=ISO-8859-1"))
+        .andReturn();
     }
 
 }
