991814ddcfefcb8bf085c8d58ebe6738ff227883
Pawel <pawel.grzyb1985@gmail.com>
2018-07-25 22:07:46 +0200

    simplified factory for blind beans

--- src/main/java/com/pi4home/configurations/FactoryBeanAppConfig.java
+++ src/main/java/com/pi4home/configurations/FactoryBeanAppConfig.java
@@ -1,11 +1,11 @@
 package com.pi4home.configurations;
 
-import com.pi4home.model.Blind;
-import com.pi4home.providers.BlindsFactory;
+import com.pi4home.blinds.Blind;
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,88 +15,64 @@ public class FactoryBeanAppConfig
     //ToDo: create wraper for RaspiPin to get a pin usage, if it is set in some other object, shoud throw an exception
     GpioController gpioController = GpioFactory.getInstance();
 
+    @Autowired
+    Blind blind1;
+    @Autowired
+    Blind blind2;
+    @Autowired
+    Blind blind3;
+    @Autowired
+    Blind blind4;
+    @Autowired
+    Blind blind5;
+
     @Bean(name = "blindLargeWindowLeft")
-    public BlindsFactory blindFactoryLargeWindowLeft()
+    public Blind blindFactoryLargeWindowLeft()
     {
-        BlindsFactory blindsFactory = new BlindsFactory();
-        blindsFactory.setName("largeWindowLeft");
-        blindsFactory.setGoUpPin(gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_01, "largeWindowLeftUp", PinState.LOW));
-        blindsFactory.setGoDownPin(gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_02, "largeWindowLeftDown", PinState.LOW));
+        blind1.setName("largeWindowLeft");
+        blind1.setGoUpPin(gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_01, "largeWindowLeftUp", PinState.LOW));
+        blind1.setGoDownPin(gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_02, "largeWindowLeftDown", PinState.LOW));
 
-        return blindsFactory;
-    }
-
-    @Bean
-    public Blind blindLargeWindowLeft() throws Exception
-    {
-        return blindFactoryLargeWindowLeft().getObject();
+        return blind1;
     }
 
     @Bean(name = "blindLargeWindowRight")
-    public BlindsFactory blindFactoryLargeWindowRight()
+    public Blind blindFactoryLargeWindowRight()
     {
-        BlindsFactory blindsFactory = new BlindsFactory();
-        blindsFactory.setName("largeWindowRight");
-        blindsFactory.setGoUpPin(gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_03, "largeWindowRightUp", PinState.LOW));
-        blindsFactory.setGoDownPin(gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_04, "largeWindowRightDown", PinState.LOW));
+        blind2.setName("largeWindowRight");
+        blind2.setGoUpPin(gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_03, "largeWindowRightUp", PinState.LOW));
+        blind2.setGoDownPin(gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_04, "largeWindowRightDown", PinState.LOW));
 
-        return blindsFactory;
-    }
-
-    @Bean
-    public Blind blindLargeWindowRight() throws Exception
-    {
-        return blindFactoryLargeWindowRight().getObject();
+        return blind2;
     }
 
     @Bean(name = "blindSmallWindowLeft")
-    public BlindsFactory blindFactorySmallWindowLeft()
+    public Blind blindFactorySmallWindowLeft()
     {
-        BlindsFactory blindsFactory = new BlindsFactory();
-        blindsFactory.setName("smallWindowLeft");
-        blindsFactory.setGoUpPin(gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_05, "smallWindowLeftUp", PinState.LOW));
-        blindsFactory.setGoDownPin(gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_06, "smallWindowLeftDown", PinState.LOW));
+        blind3.setName("smallWindowLeft");
+        blind3.setGoUpPin(gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_05, "smallWindowLeftUp", PinState.LOW));
+        blind3.setGoDownPin(gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_06, "smallWindowLeftDown", PinState.LOW));
 
-        return blindsFactory;
-    }
-
-    @Bean
-    public Blind blindSmallWindowLeft() throws Exception
-    {
-        return blindFactorySmallWindowLeft().getObject();
+        return blind3;
     }
 
     @Bean(name = "blindSmallWindowMiddle")
-    public BlindsFactory blindFactorySmallWindowMiddle()
+    public Blind blindFactorySmallWindowMiddle()
     {
-        BlindsFactory blindsFactory = new BlindsFactory();
-        blindsFactory.setName("smallWindowMiddle");
-        blindsFactory.setGoUpPin(gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_07, "smallWindowMiddleUp", PinState.LOW));
-        blindsFactory.setGoDownPin(gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_08, "smallWindowMiddleDown", PinState.LOW));
+        blind4.setName("smallWindowMiddle");
+        blind4.setGoUpPin(gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_07, "smallWindowMiddleUp", PinState.LOW));
+        blind4.setGoDownPin(gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_08, "smallWindowMiddleDown", PinState.LOW));
 
-        return blindsFactory;
-    }
-
-    @Bean
-    public Blind blindSmallWindowMiddle() throws Exception
-    {
-        return blindFactorySmallWindowMiddle().getObject();
+        return blind4;
     }
 
     @Bean(name = "blindSmallWindowRight")
-    public BlindsFactory blindFactorySmallWindowRight()
+    public Blind blindFactorySmallWindowRight()
     {
-        BlindsFactory blindsFactory = new BlindsFactory();
-        blindsFactory.setName("smallWindowRight");
-        blindsFactory.setGoUpPin(gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_09, "smallWindowRightUp", PinState.LOW));
-        blindsFactory.setGoDownPin(gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_10, "smallWindowRightDown", PinState.LOW));
+        blind5.setName("smallWindowRight");
+        blind5.setGoUpPin(gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_09, "smallWindowRightUp", PinState.LOW));
+        blind5.setGoDownPin(gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_10, "smallWindowRightDown", PinState.LOW));
 
-        return blindsFactory;
-    }
-
-    @Bean
-    public Blind blindSmallWindowRight() throws Exception
-    {
-        return blindFactorySmallWindowRight().getObject();
+        return blind5;
     }
 }
