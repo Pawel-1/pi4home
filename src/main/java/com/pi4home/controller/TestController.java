3f0b187036187abdc55ca60dc287833e0d7f5855
Pawel <pawel.grzyb1985@gmail.com>
2018-07-31 22:08:37 +0200

    added test for pin

--- src/main/java/com/pi4home/controller/TestController.java
+++ src/main/java/com/pi4home/controller/TestController.java
@@ -9,20 +9,23 @@ public class TestController
 {
     final GpioController gpio = GpioFactory.getInstance();
     final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_16, "MyLED", PinState.HIGH);
+    final GpioPinDigitalOutput pin2 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05, "MyLED", PinState.HIGH);
 
     @RequestMapping("/test")
     public String root() throws InterruptedException
     {
         System.out.println("Root for simple E2E testing");
 
-
         pin.setShutdownOptions(true, PinState.LOW);
+        pin2.setShutdownOptions(true, PinState.LOW);
 
         System.out.println("--> GPIO state should be: ON");
 
         Thread.sleep(5000);
 
         pin.low();
+        pin2.low();
+
         System.out.println("--> GPIO state should be: OFF");
 
         Thread.sleep(5000);
