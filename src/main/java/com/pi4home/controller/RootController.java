dcac30aa11083ba0313b1167dca171ed10b52863
Pawel <pawel.grzyb1985@gmail.com>
2018-09-20 21:20:39 +0200

    updated spring boot version, commented out rootController to test if index.html works fine

--- src/main/java/com/pi4home/controller/RootController.java
+++ src/main/java/com/pi4home/controller/RootController.java
@@ -1,31 +1,31 @@
-package com.pi4home.controller;
-
-import com.pi4home.services.BlindsService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.stream.Collectors;
-
-@RestController
-public class RootController
-{
-    @Autowired
-    private BlindsService blindsService;
-
-    @RequestMapping("/")
-    public String root()
-    {
-        return blindsService.getBlindList()
-                .stream()
-                .collect(Collectors.toMap(blind -> blind.getName(),
-                        blind -> blind.getBlindState()))
-                .entrySet()
-                .stream()
-                .map(e ->
-                {
-                    return e.getKey() + "  |  " + e.getValue() + ", " + "\n";
-                })
-                .collect(Collectors.joining("\n"));
-    }
-}
+//package com.pi4home.controller;
+//
+//import com.pi4home.services.BlindsService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.stream.Collectors;
+//
+//@RestController
+//public class RootController
+//{
+//    @Autowired
+//    private BlindsService blindsService;
+//
+//    @RequestMapping("/")
+//    public String root()
+//    {
+//        return blindsService.getBlindList()
+//                .stream()
+//                .collect(Collectors.toMap(blind -> blind.getName(),
+//                        blind -> blind.getBlindState()))
+//                .entrySet()
+//                .stream()
+//                .map(e ->
+//                {
+//                    return e.getKey() + "  |  " + e.getValue() + ", " + "\n";
+//                })
+//                .collect(Collectors.joining("\n"));
+//    }
+//}
