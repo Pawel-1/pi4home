6f4321eb9a38e53e7c453a1f9994a27f63125ce7
Pawel <pawel.grzyb1985@gmail.com>
2018-09-20 21:56:21 +0200

    added blinds web layout and new request mapping

--- src/main/java/com/pi4home/controller/BlindsController.java
+++ src/main/java/com/pi4home/controller/BlindsController.java
@@ -2,6 +2,7 @@ package com.pi4home.controller;
 
 import com.pi4home.services.BlindsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,12 @@ public class BlindsController
     @Autowired
     private BlindsService blindsService;
 
+    @ModelAttribute
+    @RequestMapping("/blinds")
+    public String blinds()
+    {
+        return "blinds";
+    }
 
     @RequestMapping("/blinds/{name}")
     public String blinds(@PathVariable String name)
