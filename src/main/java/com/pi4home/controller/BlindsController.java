d216fcd7b1ddd2d087de78dd979d424398fde1ec
Pawel <pawel.grzyb1985@gmail.com>
2018-09-20 22:13:24 +0200

    changed annotation, for test only

--- src/main/java/com/pi4home/controller/BlindsController.java
+++ src/main/java/com/pi4home/controller/BlindsController.java
@@ -3,6 +3,7 @@ package com.pi4home.controller;
 import com.pi4home.blinds.Blind;
 import com.pi4home.services.BlindsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class BlindsController
 {
     @Autowired
