f2178a351066913b7e7e4fb24c57733b7b75982d
Pawel <pawel.grzyb1985@gmail.com>
2018-09-20 22:07:32 +0200

    added blindList to the model, updated blinds.html to handle with a list of model attributes

--- src/main/java/com/pi4home/controller/BlindsController.java
+++ src/main/java/com/pi4home/controller/BlindsController.java
@@ -1,11 +1,14 @@
 package com.pi4home.controller;
 
+import com.pi4home.blinds.Blind;
 import com.pi4home.services.BlindsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class BlindsController
@@ -13,10 +16,13 @@ public class BlindsController
     @Autowired
     private BlindsService blindsService;
 
-    @ModelAttribute
     @RequestMapping("/blinds")
-    public String blinds()
+    public String blinds(Model model)
     {
+        List<Blind> blindList = blindsService.getBlindList();
+        model.addAttribute("blindList", blindList);
+
+
         return "blinds";
     }
 
