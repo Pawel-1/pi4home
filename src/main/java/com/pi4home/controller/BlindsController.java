fcb717fcecd54096925a0b1638af983263095764
Pawel <pawel.grzyb1985@gmail.com>
2018-09-22 21:36:08 +0200

    changed controller to RestController, to return json instead of html content

--- src/main/java/com/pi4home/controller/BlindsController.java
+++ src/main/java/com/pi4home/controller/BlindsController.java
@@ -3,14 +3,14 @@ package com.pi4home.controller;
 import com.pi4home.blinds.Blind;
 import com.pi4home.services.BlindsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class BlindsController
 {
     @Autowired
