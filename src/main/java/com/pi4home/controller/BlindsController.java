7daae9d94616181fd1aab2927e64183ae6758b1b
Pawel <pawel.grzyb1985@gmail.com>
2018-11-26 22:24:15 +0100

    deleted unused import

--- src/main/java/com/pi4home/controller/BlindsController.java
+++ src/main/java/com/pi4home/controller/BlindsController.java
@@ -3,7 +3,6 @@ package com.pi4home.controller;
 import com.pi4home.model.blinds.Blind;
 import com.pi4home.services.BlindsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
