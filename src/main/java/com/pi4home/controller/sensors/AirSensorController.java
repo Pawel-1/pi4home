bb24efa29080af1b4fb75c8a0b1890589c483328
Pawel <pawel.grzyb1985@gmail.com>
2018-11-27 22:39:07 +0100

    added save to DB after every call to airSensor endpoint

--- src/main/java/com/pi4home/controller/sensors/AirSensorController.java
+++ src/main/java/com/pi4home/controller/sensors/AirSensorController.java
@@ -1,5 +1,6 @@
 package com.pi4home.controller.sensors;
 
+import com.pi4home.jpa.SensorRepository;
 import com.pi4home.restResources.Sensors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,10 +17,17 @@ public class AirSensorController
     @Autowired
     RestTemplate restTemplate;
 
+    @Autowired
+    SensorRepository sensorRepository;
+
     @RequestMapping("/airSensor")
     public Sensors readFromAirSensors()
     {
         Sensors sensors = restTemplate.getForObject(url, Sensors.class);
+
+        sensors.getSensors()
+                .forEach(sensorRepository::save);
+
         return sensors;
     }
 
