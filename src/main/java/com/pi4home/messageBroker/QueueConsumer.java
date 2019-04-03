1771f59c6a87eecc78f4849af252ade9d9d66ae0
Pawel <pawel.grzyb1985@gmail.com>
2019-04-03 20:48:41 +0200

    try for new way of processing messages from queue to handle more than one object only

--- src/main/java/com/pi4home/messageBroker/QueueConsumer.java
+++ src/main/java/com/pi4home/messageBroker/QueueConsumer.java
@@ -4,11 +4,14 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pi4home.model.lights.Light;
+import com.pi4home.services.BlindsService;
 import com.pi4home.services.LightsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 public class QueueConsumer
@@ -16,6 +19,9 @@ public class QueueConsumer
 
     @Autowired
     LightsService lightsService;
+
+    @Autowired
+    BlindsService blindsService;
 
     private Logger logger = LoggerFactory.getLogger(getClass());
 
@@ -32,28 +38,51 @@ public class QueueConsumer
         processMessage(strMessage);
     }
 
-    private void processMessage(String message)
+    private void processMessage(String brokerMessage)
     {
-        logger.info("Processing message from broker");
+
+        Response response = null;
         try
         {
-            Light light = new ObjectMapper().readValue(message, Light.class);
-            lightsService.handleLightDissimilarity(light);
-
-            logger.info("Message from broker consumed: " + light.getName()
-                    + " is turned on: " + light.isTurnedOn());
+            response = new ObjectMapper().readValue(brokerMessage, Response.class);
         }
         catch (JsonParseException e)
         {
-            logger.warn("Bad JSON in message: " + message);
+            logger.warn("Bad JSON in message: " + brokerMessage, e.getMessage());
         }
         catch (JsonMappingException e)
         {
-            logger.warn("cannot map JSON to NotificationRequest: " + message);
+            logger.warn("cannot map JSON to NotificationRequest: " + brokerMessage, e.getMessage());
         }
         catch (Exception e)
         {
             logger.error(e.getMessage());
         }
+        if(response.response instanceof Light)
+        {
+            lightsService.handleLightDissimilarity((Light)response.response);
+        }
+
+        logger.info("Processing message from broker");
+//        try
+//        {
+//            Light light = new ObjectMapper().readValue(brokerMessage, Light.class);
+//            lightsService.handleLightDissimilarity(light);
+//
+//            logger.info("Message from broker consumed: " + light.getName()
+//                    + " is turned on: " + light.isTurnedOn());
+//        }
+//        catch (JsonParseException e)
+//        {
+//            logger.warn("Bad JSON in message: " + brokerMessage, e.getMessage());
+//        }
+//        catch (JsonMappingException e)
+//        {
+//            logger.warn("cannot map JSON to NotificationRequest: " + brokerMessage, e.getMessage());
+//        }
+//        catch (Exception e)
+//        {
+//            logger.error(e.getMessage());
+//        }
     }
 }
