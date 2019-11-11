06c89f8cdeefc98047e343ea7a2aebb5d9468b2c
Pawel <pawel.grzyb1985@gmail.com>
2019-11-11 22:00:20 +0100

    separate queue consumer for blinds and lights

--- src/main/java/com/pi4home/messageBroker/LightsQueueConsumer.java
+++ src/main/java/com/pi4home/messageBroker/LightsQueueConsumer.java
@@ -2,9 +2,7 @@ package com.pi4home.messageBroker;
 
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.pi4home.model.blinds.Blind;
 import com.pi4home.model.lights.Light;
-import com.pi4home.services.BlindsService;
 import com.pi4home.services.LightsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,66 +12,52 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
-public class QueueConsumer
+public class LightsQueueConsumer
 {
 
     @Autowired
     LightsService lightsService;
 
-    @Autowired
-    BlindsService blindsService;
-
     private Logger logger = LoggerFactory.getLogger(getClass());
 
-    public void receiveMessage(String message)
+    public void receiveMessageLightsQueue(String message)
     {
         logger.info("Received (String) " + message);
         processMessage(message);
     }
 
-    public void receiveMessage(byte[] message)
+    public void receiveMessageLightsQueue(byte[] message)
     {
         String strMessage = new String(message);
         logger.info("Received (No String) " + strMessage);
         processMessage(strMessage);
     }
 
+
     private void processMessage(String brokerMessage)
     {
         ObjectMapper objectMapper = new ObjectMapper();
+
+        Light light = null;
+
         try
         {
-            Light light = objectMapper.readValue(brokerMessage, Light.class);
-            lightsService.updateLightState(light);
-
-            logger.info("Message from broker consumed: " + light.getName()
-                    + " is turned on: " + light.isTurnedOn());
+            light = objectMapper.readValue(brokerMessage, Light.class);
         }
 
         catch (JsonMappingException e)
         {
             logger.warn("cannot map JSON to NotificationRequest: " + brokerMessage, e.getMessage());
+        }
 
-            try
-            {
-                Blind blind = objectMapper.readValue(brokerMessage, Blind.class);
-                try
-                {
-                    blindsService.updateBlindState(blind);
-                }
-                catch (InterruptedException e1)
-                {
-                    e1.printStackTrace();
-                }
-            }
-            catch (IOException e1)
-            {
-                e1.printStackTrace();
-            }
-        }
-        catch (Exception e)
+        catch (IOException e1)
         {
-            logger.error(e.getMessage());
+            e1.printStackTrace();
         }
+
+        lightsService.updateLightState(light);
+
+        logger.info("Message from broker consumed: " + light.getName()
+                + " is turned on: " + light.isTurnedOn());
     }
 }
