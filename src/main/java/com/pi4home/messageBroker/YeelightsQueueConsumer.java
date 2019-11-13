3e53caf8751d1eea252100d7f7f5d641ecb3165a
Pawel <pawel.grzyb1985@gmail.com>
2019-11-13 22:58:02 +0100

    Revert: added yeelight model and service implementation

--- src/main/java/com/pi4home/messageBroker/YeelightsQueueConsumer.java
+++ src/main/java/com/pi4home/messageBroker/YeelightsQueueConsumer.java
@@ -1,24 +1,15 @@
 package com.pi4home.messageBroker;
 
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.pi4home.model.yeelight.Yeelight;
-import com.pi4home.services.YeelightService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
-
-import java.io.IOException;
 
 @PropertySource(ignoreResourceNotFound = true, value = "classpath:application.properties")
 @Component
 public class YeelightsQueueConsumer
 {
 
-    @Autowired
-    YeelightService yeelightService;
 
     private Logger logger = LoggerFactory.getLogger(getClass());
 
@@ -38,25 +29,6 @@ public class YeelightsQueueConsumer
 
     private void processMessage(String brokerMessage)
     {
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        Yeelight yeelight = null;
-
-        try
-        {
-            yeelight = objectMapper.readValue(brokerMessage, Yeelight.class);
-        }
-        catch (JsonMappingException e)
-        {
-            logger.warn("cannot map JSON to NotificationRequest: " + brokerMessage, e.getMessage());
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-
-        yeelightService.updateYeelightStatus(yeelight);
-
-        logger.info("Message from broker consumed: " + yeelight.getName()
-                + " is turned on: " + yeelight.isTurnedOn());    }
+        logger.warn("GET Yeelight message" + brokerMessage);
+    }
 }
