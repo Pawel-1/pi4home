3109ea7dfe125678b6f889525cfb66b79e9cf4ac
Pawel <pawel.grzyb1985@gmail.com>
2019-11-13 11:52:40 +0100

    added yeelight consumption from queue server

---
+++ src/main/java/com/pi4home/messageBroker/YeelightsQueueConsumer.java
@@ -0,0 +1,34 @@
+package com.pi4home.messageBroker;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@PropertySource(ignoreResourceNotFound = true, value = "classpath:application.properties")
+@Component
+public class YeelightsQueueConsumer
+{
+
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    public void receiveMessageLights(String message)
+    {
+        logger.info("Received (String) " + message);
+        processMessage(message);
+    }
+
+    public void receiveMessageLights(byte[] message)
+    {
+        String strMessage = new String(message);
+        logger.info("Received (No String) " + strMessage);
+        processMessage(strMessage);
+    }
+
+
+    private void processMessage(String brokerMessage)
+    {
+        logger.warn("GET Yeelight message" + brokerMessage);
+    }
+}
