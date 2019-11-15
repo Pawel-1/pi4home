9be1a4cf18a58cf93ccfeaab4eade73a2625157d
Pawel <pawel.grzyb1985@gmail.com>
2019-11-15 21:11:35 +0100

    Revert: Revert: added yeelight model and service implementation

---
+++ src/main/java/com/pi4home/configurations/YeelightFactoryBeanAppConfig.java
@@ -0,0 +1,72 @@
+package com.pi4home.configurations;
+
+import com.mollin.yapi.YeelightDevice;
+import com.mollin.yapi.exception.YeelightSocketException;
+import com.pi4home.model.yeelight.YeelightDeviceWrapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class YeelightFactoryBeanAppConfig
+{
+
+    @Bean(name = "deskYeelight")
+    public YeelightDeviceWrapper deskYeelight()
+    {
+        YeelightDevice deskLightYeelightDevice = null;
+        try
+        {
+            deskLightYeelightDevice = new YeelightDevice("192.168.0.192");
+        }
+        catch (YeelightSocketException e)
+        {
+            e.printStackTrace();
+        }
+
+        YeelightDeviceWrapper deskLightYeelightDeviceWrapper = new YeelightDeviceWrapper();
+        deskLightYeelightDeviceWrapper.setYeelightDevice(deskLightYeelightDevice);
+        deskLightYeelightDeviceWrapper.setName("deskYeelight");
+
+        return deskLightYeelightDeviceWrapper;
+    }
+
+    @Bean(name = "rightYeelight")
+    public YeelightDeviceWrapper rightYeelight()
+    {
+        YeelightDevice rightYeelightDevice = null;
+        try
+        {
+            rightYeelightDevice = new YeelightDevice("192.168.0.191");
+        }
+        catch (YeelightSocketException e)
+        {
+            e.printStackTrace();
+        }
+
+        YeelightDeviceWrapper deskLightYeelightDeviceWrapper = new YeelightDeviceWrapper();
+        deskLightYeelightDeviceWrapper.setYeelightDevice(rightYeelightDevice);
+        deskLightYeelightDeviceWrapper.setName("rightYeelight");
+
+        return deskLightYeelightDeviceWrapper;
+    }
+
+    @Bean(name = "leftYeelight")
+    public YeelightDeviceWrapper leftYeelight()
+    {
+        YeelightDevice leftYeelightDevice = null;
+        try
+        {
+            leftYeelightDevice = new YeelightDevice("192.168.0.190");
+        }
+        catch (YeelightSocketException e)
+        {
+            e.printStackTrace();
+        }
+
+        YeelightDeviceWrapper deskLightYeelightDeviceWrapper = new YeelightDeviceWrapper();
+        deskLightYeelightDeviceWrapper.setYeelightDevice(leftYeelightDevice);
+        deskLightYeelightDeviceWrapper.setName("leftYeelight");
+
+        return deskLightYeelightDeviceWrapper;
+    }
+}
